fabf86e6203be1b0afca2f73a9f4a4a1e6ffecb2
Serdar Sutay <serdar@opscode.com>
2016-02-16 10:18:11 -0800

    Refactoring and cleanup.

--- lib/lita/handlers/versioner.rb
+++ lib/lita/handlers/versioner.rb
@@ -1,4 +1,5 @@
 require "json"
+require "uri"
 
 module Lita
   module Handlers
@@ -11,72 +12,106 @@ module Lita
         "build PIPELINE <TAG>" => "Kicks off a build for PIPELINE with TAG. TAG default: master"
       })
 
+      PROJECTS = {
+        harmony: {
+          pipeline: "harmony-trigger-ad_hoc",
+          repository: "lita-test" # For testing purposes we are watching this repository.
+        }
+        # chef: {
+        #   pipeline: "chef-trigger-release",
+        #   repository: "chef"
+        # }
+      }
+
       def build(response)
         params = response.args
         unless params.length == 1 || params.length == 2
-          log "Argument issue. You can run this command like 'build PIPELINE <TAG>'"
+          response.reply("Argument issue. You can run this command like 'build PROJECT <TAG>'.")
           return
         end
 
-        pipeline_name = params.shift
+        project = params.shift.to_sym
         build_tag = params.shift || "master"
-        log "Kicking off a build for #{pipeline_name} with #{build_tag}."
 
-        status = rest_post(
-          "GIT_REF" => build_tag,
-          "EXPIRE_CACHE" => false
-        )
-
-        if status
-          response.reply "Done!"
-        else
-          response.reply "Sorry, there was an error when kicking off the build!"
-        end
-      end
-
-      ENDPOINT = "http://manhattan.ci.chef.co/".freeze
-
-      def rest_post(parameters)
-        uri = URI.parse(ENDPOINT)
-        http = Net::HTTP.new(uri.host, uri.port)
-        http.use_ssl = (uri.scheme == "https")
-
-        path = "/job/harmony-trigger-ad_hoc/buildWithParameters"
-        full_path = [path, URI.encode_www_form(parameters)].join("?")
-        request = Net::HTTP::Post.new(full_path)
-        request.basic_auth(config.jenkins_username, config.jenkins_api_token)
-        request["Accept"] = "application/json"
-
-        res = http.request(request)
-
-        begin
-          # Raise if response is not 2XX
-          res.value
-        rescue
-          return false
+        if PROJECTS[project].nil?
+          response.reply("Project '#{project}' is not supported yet!")
+          return
         end
 
-        true
+        trigger_build(PROJECTS[project][:pipeline], build_tag)
       end
 
       def github_handler(request, response)
         payload = JSON.parse(request.params["payload"])
 
         event_type = request.env["HTTP_X_GITHUB_EVENT"]
-        log "Processing '#{event_type}' event for '#{payload["repository"]["name"]}'."
+        repository = payload["repository"]["name"]
+        log "Processing '#{event_type}' event for '#{repository}'."
+
+        target_pipeline = nil
+        PROJECTS.each do |project, project_data|
+          if project_data[:repository] == repository
+            log "Found matching project '#{project}'"
+            target_pipeline = project_data[:pipeline]
+            break
+          end
+        end
+
+        if target_pipeline.nil?
+          log "Repository '#{repository}' is not monitored!"
+          return
+        end
 
         case event_type
-        # https://developer.github.com/v3/activity/events/types/#pullrequestevent
         when "pull_request"
+          # https://developer.github.com/v3/activity/events/types/#pullrequestevent
           # If the pull request is merged with some commits
           if payload["action"] == "closed" && payload["pull_request"]["merged"]
             log "Pull request '#{payload["pull_request"]["url"]}' is merged."
+            # TODO: Update the version, tag and commit.
+            # TODO: Kick off build using the tag we created
+            trigger_build(target_pipeline, "master")
           else
-            log "Skipping"
+            log "Skipping..."
           end
         else
           log "Skipping..."
         end
+      end
+
+      private
+      def trigger_build(pipeline, tag)
+        log "Kicking off a build for #{pipeline} with #{tag}."
+
+        status = rest_post("/job/#{pipeline}/buildWithParameters",
+          "GIT_REF" => tag,
+          "EXPIRE_CACHE" => false
+        )
+
+        begin
+          # Raise if response is not 2XX
+          status.value
+        rescue
+          log "Sorry, there was an error when kicking off the build!"
+          log status.body
+          return
+        end
+
+        log "Done!"
+      end
+
+      JENKINS_ENDPOINT = "http://manhattan.ci.chef.co/".freeze
+      def rest_post(path, parameters)
+        uri = URI.parse(JENKINS_ENDPOINT)
+        http = Net::HTTP.new(uri.host, uri.port)
+        http.use_ssl = (uri.scheme == "https")
+
+        full_path = [path, URI.encode_www_form(parameters)].join("?")
+        request = Net::HTTP::Post.new(full_path)
+        request.basic_auth(config.jenkins_username, config.jenkins_api_token)
+        request["Accept"] = "application/json"
+
+        http.request(request)
       end
 
       def log(message)
